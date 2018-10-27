158cca610a77c333eab21e76ab8e4ef466e04fa7
BeGieU <marcinoo890@gmail.com>
2018-10-27 13:52:09 +0200

    01 -after episode 107 - adding index page

---
+++ src/main/java/com/example/recipeproject/controllers/IndexController.java
@@ -0,0 +1,19 @@
+package com.example.recipeproject.controllers;
+/*
+Author: BeGieU
+Date: 27.10.2018
+*/
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController
+{
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage()
+    {
+        return "index";
+    }
+
+}
