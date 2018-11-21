a76f1515ac365bfc374d2d1f43ebbab041d48aa0
BeGieU <marcinoo890@gmail.com>
2018-11-21 21:45:10 +0100

    10 - refactored to use lombok

--- src/main/java/com/example/recipeproject/controllers/IndexController.java
+++ src/main/java/com/example/recipeproject/controllers/IndexController.java
@@ -5,14 +5,14 @@ Date: 27.10.2018
 */
 
 import com.example.recipeproject.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-//TODO fix that index page doesn't show recipes !!!
-
 //TODO add java configuration
 
+@Slf4j
 @Controller
 public class IndexController
 {
@@ -28,6 +28,7 @@ public class IndexController
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model)
     {
+        log.debug("Getting index Page");
         model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
