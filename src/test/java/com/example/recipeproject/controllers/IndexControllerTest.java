7c715252466f160e442660951ab1ea3008b2f439
BeGieU <marcinoo890@gmail.com>
2018-11-24 13:02:07 +0100

    added index controller test

--- src/test/java/com/example/recipeproject/controllers/IndexControllerTest.java
+++ src/test/java/com/example/recipeproject/controllers/IndexControllerTest.java
@@ -6,31 +6,41 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.springframework.ui.Model;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class IndexControllerTest
 {
-
     @Mock
-    private  RecipeService recipeService;
+    private RecipeService recipeService;
 
     @Mock
     private Model model;
 
     private IndexController indexController;
 
-
-
     @Before
     public void setUp() throws Exception
     {
-       initMocks(this);
-       indexController=new IndexController(recipeService);
+        initMocks(this);
+        indexController = new IndexController(recipeService);
+
+
     }
 
     @Test
     public void getIndexPage()
     {
-//      );
+        assertEquals(indexController.getIndexPage(model),
+                "index");
+
+        verify(recipeService, times(1)).getRecipes();
+
+        verify(model, times(1))
+                .addAttribute(eq("recipes"), anySet());
     }
 }
