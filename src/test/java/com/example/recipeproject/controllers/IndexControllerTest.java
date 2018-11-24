24674b4861d49e6bb47a8f4d7ba92f75b20310de
BeGieU <marcinoo890@gmail.com>
2018-11-24 14:46:46 +0100

    extended index controller test

--- src/test/java/com/example/recipeproject/controllers/IndexControllerTest.java
+++ src/test/java/com/example/recipeproject/controllers/IndexControllerTest.java
@@ -1,16 +1,18 @@
 package com.example.recipeproject.controllers;
 
+import com.example.recipeproject.model.Recipe;
 import com.example.recipeproject.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anySet;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class IndexControllerTest
@@ -35,12 +37,42 @@ public class IndexControllerTest
     @Test
     public void getIndexPage()
     {
-        assertEquals(indexController.getIndexPage(model),
-                "index");
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
 
+        /*w jednej recipe trzeba ustawic id zeby recipy sie roznily
+        bo hash set nie dodaje takich samych biektow*/
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
+        String viewName = indexController.getIndexPage(model);
+
+
+        //then
+        assertEquals("index", viewName);
         verify(recipeService, times(1)).getRecipes();
 
-        verify(model, times(1))
-                .addAttribute(eq("recipes"), anySet());
+        /* argumentCaptor.capture() lapie argument ktory zostal podany
+         * w miejscu w ktorym sie znajduje to wywolanie, czyli
+         * addAttribute(eq("recipes"), argumentCaptor.capture())
+         * w tym wypadku lapie to co jest argumentem numer 2 we funkcji add atrbute mianowicie
+         * model.addAttribute("recipes", recipeService.getRecipes()) w klasie IndexController;
+         * czyli po prostu zlapal recipeService.getRecipes().
+         * Caly zapis ponizszego wywolania znaczy odpal metode z modelu o nazwie addatribute
+         * z danymi argumentami i zlap argument numer 2*/
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+
+        /*tutaj zlapana wartosc przypisujemy do seta*/
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        /*i sprawdzamy czy set ktory jest zlapany w index kontrloerze faktycznie ma 2 argumenty*/
+        assertEquals(2, setInController.size());
     }
 }
