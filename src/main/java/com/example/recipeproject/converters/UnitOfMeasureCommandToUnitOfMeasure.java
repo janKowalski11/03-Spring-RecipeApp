964ed3e948688e193581209888803693faaa1b5a
BeGieU <marcinoo890@gmail.com>
2018-12-16 14:02:41 +0100

    added UomCommandToUom converter and it's tests

---
+++ src/main/java/com/example/recipeproject/converters/UnitOfMeasureCommandToUnitOfMeasure.java
@@ -0,0 +1,34 @@
+package com.example.recipeproject.converters;
+/*
+Author: BeGieU
+Date: 16.12.2018
+*/
+
+import com.example.recipeproject.commands.UnitOfMeasureCommand;
+import com.example.recipeproject.model.UnitOfMeasure;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UnitOfMeasureCommandToUnitOfMeasure implements Converter<UnitOfMeasureCommand, UnitOfMeasure>
+{
+
+    @Synchronized
+    @Nullable
+    @Override
+    public UnitOfMeasure convert(UnitOfMeasureCommand source)
+    {
+        if (source == null)
+        {
+            return null;
+        }
+
+        final UnitOfMeasure uom=new UnitOfMeasure();
+        uom.setId(source.getId());
+        uom.setDescription(source.getDescription());
+
+        return uom;
+    }
+}
